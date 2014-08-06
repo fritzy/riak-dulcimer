1ee3e70149af969c36926e05a86432bb05baf478
Nathan Fritz <fritzy@netflint.net>
2014-08-06 11:01:19 -0700

    update to actually use levelout

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var LevelBucketArray = require('level-bucketarray');
-var levelup = require('levelup');
+var levelout = require('levelout');
 var riakdown = require('riakdown');
 var RiakDownIndexTotal = require('riakdown-indextotal');
 var LevelForeignKeys = require('level-foreignkeys');
@@ -7,7 +7,7 @@ var RiakDownIncrement = require('riakdown-increment');
 
 function RiakDulcimer(uri, opts) {
     console.log("Connecting to", uri);
-    return LevelForeignKeys(RiakDownIncrement(LevelBucketArray(RiakDownIndexTotal(levelup(uri, {db: require('riakdown'), valueEncoding: 'json'})))));
+    return LevelForeignKeys(RiakDownIncrement(LevelBucketArray(RiakDownIndexTotal(levelout(uri, {db: require('riakdown'), valueEncoding: 'json'})))));
 }
 
 module.exports = RiakDulcimer;
