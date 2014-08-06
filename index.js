2a5105eba7e74291c7183cb27ebe551938e29548
Nathan Fritz <fritzy@netflint.net>
2014-08-06 00:27:26 -0700

    add more mixins

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var LevelBucketArray = require('level-bucketarray');
 var levelup = require('levelup');
 var riakdown = require('riakdown');
 var RiakDownIndexTotal = require('riakdown-indextotal');
+var LevelForeignKeys = require('level-foreignkeys');
+var RiakDownIncrement = require('riakdown-increment');
 
 function RiakDulcimer(uri, opts) {
     console.log("Connecting to", uri);
-    return LevelBucketArray(RiakDownIndexTotal(levelup(uri, {db: require('riakdown'), valueEncoding: 'json'})));
+    return LevelForeignKeys(RiakDownIncrement(LevelBucketArray(RiakDownIndexTotal(levelup(uri, {db: require('riakdown'), valueEncoding: 'json'})))));
 }
 
 module.exports = RiakDulcimer;
